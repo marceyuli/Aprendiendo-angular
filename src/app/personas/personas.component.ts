a53c14bb40552d1b242d150e685a0abca2342d7d
Yuliana <yulianamarcela200@gmail.com>
2021-10-11 19:00:45 -0400

    Event binding input

--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -8,8 +8,13 @@ import { Component } from "@angular/core";
 export class PersonasComponent{
     deshabilitar= false;
     mensaje ='No se ha agregado ninguna persona'
+    titulo = '';
 
     agregarPersona(){
         this.mensaje = 'Persona agregada'
+    }
+
+    modificarTitulo(event: Event){
+        this.titulo = (<HTMLInputElement>event.target).value;
     }
 }
