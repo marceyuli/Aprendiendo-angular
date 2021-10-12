30bc837f90f9a42addf74b935c05bcdf230012ca
Yuliana <yulianamarcela200@gmail.com>
2021-10-11 20:01:42 -0400

    two way binding input

--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -8,13 +8,10 @@ import { Component } from "@angular/core";
 export class PersonasComponent{
     deshabilitar= false;
     mensaje ='No se ha agregado ninguna persona'
-    titulo = '';
+    titulo = 'Ingeniero';
 
     agregarPersona(){
         this.mensaje = 'Persona agregada'
     }
 
-    modificarTitulo(event: Event){
-        this.titulo = (<HTMLInputElement>event.target).value;
-    }
 }
