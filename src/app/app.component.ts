f2d9a91e1728c8bc14b063262dda47d1c48631b2
Yuliana <yulianamarcela200@gmail.com>
2021-10-14 20:27:52 -0400

    ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Persona } from './persona.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Hola mundo desde angular';
+  title = 'Listado de personas';
+  personas: Persona[] = [new Persona('Juan','Perez'),
+   new Persona('Laura','Juarez'),
+  new Persona('Carla', 'Lara')]
 }
