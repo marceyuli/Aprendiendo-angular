2ea8527100c269bc9372c1e39727ef33f8133626
Yuliana <yulianamarcela200@gmail.com>
2021-10-26 11:46:42 -0400

    Child route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Routes,RouterModule } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { PersonasComponent } from './personas/personas.component';
 import { FormularioComponent } from './personas/formulario/formulario.component';
 
 
-const routes: Routes =[
-  {path: '',component:PersonasComponent},
-  {path: 'personas', component: PersonasComponent},
-  {path: 'personas/agregar', component: FormularioComponent},
-  {path: 'personas/:id', component: FormularioComponent}
+const routes: Routes = [
+  { path: '', component: PersonasComponent },
+  {
+    path: 'personas', component: PersonasComponent, children: [
+      { path: 'agregar', component: FormularioComponent },
+      { path: ':id', component: FormularioComponent }
+    ]
+  }
 ]
-
 @NgModule({
-  imports: [
-    RouterModule.forRoot(
-      routes
-    )
-  ],
-  exports: [RouterModule]
-})
+    imports: [
+      RouterModule.forRoot(
+        routes
+      )
+    ],
+    exports: [RouterModule]
+  })
 export class AppRoutingModule { }
